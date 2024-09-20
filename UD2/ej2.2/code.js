f32c7c4bbf2356e2bf30af00e9126a7c7f95a9b3
Javier <javmanfer@gmail.com>
2024-09-20 22:14:04 +0200

    Ejercicios 2

---
+++ UD2/ej2.2/code.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Calcula la media de los datos
+// 4, 8, 9, 7, 2, 6
+
+let suma = 4 + 8 + 9 + 7 + 2 + 6
+let media = suma / 6;
+console.log(`La media es ${media}`);
+
---
+++ UD2/ej2.3/code.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const resfera1 = 1;
+const vesfera1 = 4/3 * Math.PI * resfera1**3;
+
+const resfera2 = 2;
+const vesfera2 = 4/3 * Math.PI * resfera2**3;
+
+console.log(vesfera1);
+console.log(vesfera2);
