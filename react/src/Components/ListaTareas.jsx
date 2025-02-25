d6219861e54bf86f187c378e064f227e91d33786
Javier <javmanfer@gmail.com>
2025-02-25 20:44:29 +0100

    Empezado el ejericioc de una lista de tareas

--- react/src/Components/ListaTareas.jsx
+++ react/src/Components/ListaTareas.jsx
@@ -3,38 +3,51 @@ import { useState, useRef } from "react";
 
 const ListaTareas = ({children})=>{
 
-    const [arrTareas, setArrTareas] = useState([]);
-    const refIntput = useRef();
+  let [iEstado,setIEstado] = useState(0); //con useState, react agrupa los rerenderizados, así que no sufriremos por esto y formará parte del estado, así que será fácilmente usable por otros componentes.
+  const i = useRef(0); //Alternativa con useRef, no provocará nunca rerenderizados ante cambios de su valor. En este caso simple es suficiente
 
-    function anadirTarea(){
-      const texto = refIntput.current.value.trim();
-      if(!texto) return;
+  const [arrTareas, setArrTareas] = useState([]);
+  const refIntput = useRef();
 
-      const nuevaTarea={
-        tarea: texto,
-        urgencia: "baja"
-      }
-
-      setArrTareas(  [...arrTareas, nuevaTarea]  );
-
+  function anadirTarea(){
+    const texto = refIntput.current.value.trim();
+    if(!texto) return;
+    i.current++; //incrementamos i
+    setIEstado(iEstado+1);
+    const nuevaTarea={
+      id: i.current,//iEstado,//Date.now(), //crypto.randomUUID()
+      tarea: texto,
+      urgencia: "baja"
     }
 
+    setArrTareas(  [...arrTareas, nuevaTarea]  );
 
-    return(<section className="mt-8 p-8">
-      {children}
-      <input ref={refIntput} type="text"  className="outline-red-500 outline-1 mr-2"/>
-      <button onClick={anadirTarea}  className="bg-red-500 py-1 px-2 cursor-pointer hover:bg-red-400 active:scale-95">Añadir Tarea</button>
+  }
 
-      <ul className="list-disc pl-8">
-        {
-          arrTareas.map( t=>{
-            return (
-              <li key={t.tarea}>{t.tarea} - Urgencia {t.urgencia}</li>
-            )})
-        } 
-      </ul>
 
-    </section>)
+  const eliminarTarea=(e)=>{
+    if(e.target.tagName!="LI") return;
+    const idEliminar = e.target.dataset.id;
+    setArrTareas(  arrTareas.filter(t=>t.id!=idEliminar) )
+  }
+
+
+  //TODO: mostrar el número de tareas
+  return(<section className="mt-8 p-8">
+    {children}
+    <input ref={refIntput} type="text"  className="outline-red-500 outline-1 mr-2"/>
+    <button onClick={anadirTarea}  className="bg-red-500 py-1 px-2 cursor-pointer hover:bg-red-400 active:scale-95">Añadir Tarea</button>
+
+    <ul onClick={eliminarTarea} className="list-disc pl-8">
+      {
+        arrTareas.map( t=>{
+          return (
+            <li key={t.id} data-id={t.id}>{t.tarea} - Urgencia {t.urgencia} - {t.id}</li>
+          )})
+      } 
+    </ul>
+
+  </section>)
 }
 
 export default ListaTareas;
---
+++ react/src/Components/ListaTareas.jsx.or
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import { useState, useRef } from "react";
+
+const ListaTareas = ({children})=>{
+
+    const [arrTareas, setArrTareas] = useState([]);
+    const refIntput = useRef();
+
+    function anadirTarea(){
+      const texto = refIntput.current.value.trim();
+      if(!texto) return;
+
+      const nuevaTarea={
+        tarea: texto,
+        urgencia: "baja"
+      }
+
+      setArrTareas(  [...arrTareas, nuevaTarea]  );
+
+    }
+
+
+    return(<section className="mt-8 p-8">
+      {children}
+      <input ref={refIntput} type="text"  className="outline-red-500 outline-1 mr-2"/>
+      <button onClick={anadirTarea}  className="bg-red-500 py-1 px-2 cursor-pointer hover:bg-red-400 active:scale-95">Añadir Tarea</button>
+
+      <ul className="list-disc pl-8">
+        {
+          arrTareas.map( t=>{
+            return (
+              <li key={t.tarea}>{t.tarea} - Urgencia {t.urgencia}</li>
+            )})
+        } 
+      </ul>
+
+    </section>)
+}
+
+export default ListaTareas;
